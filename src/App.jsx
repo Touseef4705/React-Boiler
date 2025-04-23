474365bdea1b6ebc93ea4cb679e807186bc06c49
Touseef109 <154261278+Touseef4705@users.noreply.github.com>
2025-04-23 18:41:09 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='flex justify-center items-center h-screen text-4xl'>Project Is Running With Tailwind Css</h1>
+    </>
+  )
+
+}
+
+export default App
